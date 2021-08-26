bcab4a19cd1b4614abc329f901d0f9881d10ef93
Tahmid <you@example.com>
2021-08-26 16:28:39 +0600

    init

---
+++ client/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <p>This is the frontend part of Mevn stack {{lol}}</p>
+</template>
+
+<script>
+import api from './api.js'
+
+export default {
+  data(){
+    return {
+      lol : ''
+    }
+  },
+  async created(){
+    let response = await api().get('/lol')
+    this.lol = response.data.lol
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
